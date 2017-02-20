8bad74b6584f3e291213b1fcd42c3d2e5186f454
Chris McLean <christopher.mclean@gmail.com>
2017-02-20 05:08:55 +0000

    Funcaionality completed

--- server.js
+++ server.js
@@ -1,11 +1,23 @@
 var express = require('express');
 var app = express();
 var port = process.argv[2]||process.env.PORT;
-var results = {};
+var results = {'ipaddress':null,'language':null, 'software':null};
 
 
 app.get('/', function (req, res) {
-  res.send('<H1>Header Parser Microserver</H1>');
+  var UA = req.get('User-Agent');
+  var lang = req.get('Accept-Language');
+  console.log('User-Agent - ' + UA);
+  console.log('Accept-Language - ' + lang);
+  results.software = UA.substring(UA.indexOf('(')+1,UA.indexOf(')'));
+  //check the Accept-Language string, if it has estra details (ie it contains a ,) return just the data before the comma. Otherwise use the full string
+  if(lang.indexOf(',') < 0){
+    results.language =lang;
+  } else {
+    results.language = lang.substring(0,lang.indexOf(','));
+  }
+  results.ipaddress = req.headers['x-forwarded-for'] || req.connection.remoteAddress;
+  res.send(results);
 })
 
 app.listen(port, function () {
